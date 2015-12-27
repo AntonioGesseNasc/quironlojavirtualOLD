d5c9f2fdaebbfb096151c98ea7d5f126d00f8ca5
AntonioGesseNasc <antoniogessenasc@gmail.com>
2015-12-26 23:08:24 -0200

    Início de tudo

---
+++ Quiron.LoajVirtual.Dominio/Entidades/Produto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiron.LoajVirtual.Dominio.Entidades
+{
+    public class Produto
+    {
+        public int ProdtoId { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Descricao { get; set; }
+
+        public decimal Preco { get; set; }
+
+        public string Categoria { get; set; }
+
+    }
+}
